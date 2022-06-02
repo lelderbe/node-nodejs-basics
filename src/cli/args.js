b9d1cad90d5ea750b949365807c500edcec21ee7
lelderbe <fvdc@ya.ru>
2022-06-02 19:56:12 +0300

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,19 @@
 export const parseArgs = () => {
-    // Write your code here 
+	const args = process.argv.slice(2);
+
+	const dict = {};
+	for (let i = 0; i < args.length; i = i + 2) {
+		const key = args[i].slice(2);
+		dict[key] = '';
+		if (i + 1 < args.length) {
+			dict[key] = args[i + 1];
+		}
+	}
+	
+	const outputData = [];
+	for (let key in dict) {
+		outputData.push(key + ' is ' + dict[key]);
+	}
+
+	console.log(outputData.join(', '));
 };
