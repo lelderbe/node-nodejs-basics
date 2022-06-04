e69dc8a0ee5f947ec88a89d509d42fe0edb51e11
lelderbe <fvdc@ya.ru>
2022-06-04 14:31:22 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,9 @@
+import { createReadStream } from 'fs';
+const { createHash } = await import('crypto');
+
 export const calculateHash = async () => {
-    // Write your code here 
+	const hash = createHash('sha256');
+
+	const input = createReadStream(new URL('./files/fileToCalculateHashFor.txt', import.meta.url));
+	input.pipe(hash).setEncoding('hex').pipe(process.stdout);
 };
