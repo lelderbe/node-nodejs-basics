12a3552fce6bf75eddb93a1ada42336fab089018
lelderbe <fvdc@ya.ru>
2022-06-04 10:17:10 +0300

    feat: add streams solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,7 @@
+import * as fs from 'fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+	const fd = await fs.open(new URL('./files/fileToRead.txt', import.meta.url));
+	const inputStream = fd.createReadStream({ encoding: 'utf-8' });
+	inputStream.pipe(process.stdout);
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,7 @@
+import * as fs from 'fs/promises';
+
 export const write = async () => {
-    // Write your code here 
+	const fd = await fs.open(new URL('./files/fileToWrite.txt', import.meta.url), 'a');
+	const outputStream = fd.createWriteStream({ encoding: 'utf-8' });
+	process.stdin.pipe(outputStream);
 };
