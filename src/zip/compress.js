607934e168bed10cd84f34005863fd7b2b5ca987
lelderbe <fvdc@ya.ru>
2022-06-04 12:28:15 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
+// via Callback API
 export const compress = async () => {
-    // Write your code here 
+	const source = new URL('./files/fileToCompress.txt', import.meta.url);
+	const destination = new URL('./files/archive.gz', import.meta.url)
+
+	const readStream = createReadStream(source);
+	const writeStream = createWriteStream(destination);
+	const gzip = createGzip();
+
+	pipeline(readStream, gzip, writeStream, (err) => {
+		if (err) {
+			console.error('An error occurred:', err);
+			process.exitCode = 1;
+		}
+	});
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
+import { open } from 'fs/promises';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
+// via Promises API async/await
 export const decompress = async () => {
-    // Write your code here 
+	try {
+		const source = await open(new URL('./files/archive.gz', import.meta.url), 'r');
+		const destination = await open(new URL('./files/fileToCompress.txt', import.meta.url), 'w');
+
+		const readStream = source.createReadStream();
+		const writeStream = destination.createWriteStream();
+		const gunzip = createGunzip();
+
+		const pipe = promisify(pipeline);
+		await pipe(readStream, gunzip, writeStream);
+	} catch (err) {
+		console.error('An error occurred:', err);
+		process.exitCode = 1;
+	}
 };
