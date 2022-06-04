eac60c70f8aae8a45808e4fc084ade0a998de7ce
lelderbe <fvdc@ya.ru>
2022-06-04 17:11:52 +0300

    refactor: modules refactoring done

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,12 +1,17 @@
-// const { path } = require('path');
-const { path } = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import './files/c.js';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
-let unknownObject;
+export let unknownObject;
 
 if (random > 0.5) {
     unknownObject = require('./files/a.json');
@@ -21,12 +26,6 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const createMyServer = createServerHttp((_, res) => {
+export const createMyServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
-
-module.exports = {
-    unknownObject,
-    createMyServer,
-};
-
