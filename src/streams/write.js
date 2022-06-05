9fedf2e1d0c1b53f393048269d544510ca7930d5
lelderbe <fvdc@ya.ru>
2022-06-05 12:24:54 +0300

    fix: set streams write open mode to 'w'

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,10 +1,10 @@
 import fs from 'fs/promises';
 
 export const write = async () => {
-	const fd = await fs.open(new URL('./files/fileToWrite.txt', import.meta.url), 'a');
+	const fd = await fs.open(new URL('./files/fileToWrite.txt', import.meta.url), 'w');
 	const outputStream = fd.createWriteStream({ encoding: 'utf-8' });
 	process.stdin.pipe(outputStream);
 };
 
-// for test with "npm run streams:read"
+// for test with "npm run streams:write"
 write();
