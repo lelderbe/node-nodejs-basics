cf64982327668607c61c8f2022c30ae4f31b89b1
lelderbe <fvdc@ya.ru>
2022-06-05 07:26:50 +0300

    feat: add worker solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,23 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 export const performCalculations = async () => {
-    // Write your code here
+	const cpusCount = os.cpus().length;
+	const workerFile = new URL('./worker.js', import.meta.url);
+	const workers = [];
+
+	for (let i = 0; i < cpusCount; i++) {
+		workers.push(
+			new Promise((resolve) => {
+				const worker = new Worker(workerFile, { workerData: (10 + i) });
+				worker.once('message', (data) => resolve({ status: 'resolved', data }));
+				worker.once('error', () => resolve({ status: 'error', data: null }));
+			})
+		);
+	}
+
+	return await Promise.all(workers);
 };
+
+// for test with "npm run wt"
+console.log(await performCalculations());
