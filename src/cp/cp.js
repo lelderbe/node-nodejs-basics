f619386073f773aa4ebca259b9cd7148db4ae425
lelderbe <fvdc@ya.ru>
2022-06-05 09:22:49 +0300

    feat: add cp solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,26 @@
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+	const path = new URL('./files/script.js', import.meta.url).pathname;
+	const child = spawn('node', [path, ...args]);
+
+	child.stdout.on('data', (data) => {
+		process.stdout.write(data);
+	});
+
+	child.stderr.on('data', (data) => {
+		process.stderr.write(data);
+	});
+
+	child.on('close', (code) => {
+		process.exit(code);
+	});
+
+	process.stdin.on('data', (data) => {
+		const buffer = Buffer.from(data.toString().trim());
+		child.stdin.write(buffer);
+	});
 };
+
+// for test with "npm run cp"
+spawnChildProcess(process.argv.slice(2));
